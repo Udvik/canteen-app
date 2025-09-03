64e78d6db5bae52c6121447fb021583f75eba0d7
udvik <udvik4321@gmail.com>
2025-09-03 21:32:57 +0530

    Wrote routes for orders

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+
+router.post('/', orderController.createOrder);
+router.get('/:billNumber', orderController.getOrderByBillNumber);
+router.put('/:billNumber/prepare', orderController.markPreparing);
+router.put('/:billNumber/serve', orderController.markServed);
+
+module.exports = router;
