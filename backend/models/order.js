36b4eb6d73e84fe9b90e82105079998698c32457
udvik <udvik4321@gmail.com>
2025-09-05 12:02:31 +0530

    changed the UTC timezone to IST in time fields

--- backend/models/order.js
+++ backend/models/order.js
@@ -10,8 +10,8 @@ const orderSchema = new mongoose.Schema({
   ],
   paymentStatus: { type: Boolean, default: false },
   orderStatus: { type: String, enum: ['pending', 'preparing', 'served'], default: 'pending' },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now + (5.5 * 60 * 60 * 1000) },
+  updatedAt: { type: Date, default: Date.now + (5.5 * 60 * 60 * 1000) },
 });
 
 // Middleware to update updatedAt on save
